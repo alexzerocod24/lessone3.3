961a6bbd8379d2626408117b3a3e4d4aedbe5168
alexzerocod24 <ustvich.alex@gmail.com>
2024-10-17 00:02:19 +0300

    add all cycle code

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from turtledemo.nim import SCREENWIDTH
+
 
 import pygame
 import random
@@ -12,7 +12,7 @@ pygame.display.set_caption("Игра Тир")
 icon = pygame.image.load("img/тир.jpg")
 pygame.display.set_icon(icon)
 
-target_img = pygame.image.load("img/target.png")
+target_image = pygame.image.load("img/target.png.png")
 target_width = 80
 target_height = 80
 
@@ -23,6 +23,17 @@ color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
 running = True
 while running:
-    pass
+    screen.fill(color)
+    for even in pygame.event.get():
+        if even.type == pygame.QUIT:
+            running = False
+        if even.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y + target_height:
+                target_x = random.randint(0, SCREEN_WIDTH - target_width)
+                target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+    screen.blit(target_image, (target_x, target_y))
+    pygame.display.update()
+
 
 pygame.quit()
