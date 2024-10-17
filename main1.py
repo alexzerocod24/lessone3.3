b71b02de75dd0b9bb84743e34e55e50a9b15095a
alexzerocod24 <ustvich.alex@gmail.com>
2024-10-17 20:32:59 +0300

    add the full code

--- main1.py
+++ main1.py
@@ -1,7 +1,6 @@
-
-
-import pygame
 import random
+import pygame
+
 pygame.init()
 
 SCREEN_WIDTH = 800
@@ -12,44 +11,44 @@ pygame.display.set_caption("Игра Тир")
 icon = pygame.image.load("img/тир.jpg")
 pygame.display.set_icon(icon)
 
-target_image = pygame.image.load("img/target.png.png")
+target_image = pygame.image.load("img/target.png")
 target_width = 80
 target_height = 80
 
+# Начальные координаты цели
 target_x = random.randint(0, SCREEN_WIDTH - target_width)
 target_y = random.randint(0, SCREEN_HEIGHT - target_height)
 
-color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
-
+# Начальные значения для очков и скорости движения цели
 score = 0
 target_speed_x = random.choice([-3, 3])  # Скорость по оси X
 target_speed_y = random.choice([-3, 3])  # Скорость по оси Y
 
 running = True
 while running:
-    screen.fill(color)
-    for even in pygame.event.get():
-        if even.type == pygame.QUIT:
-            running = False
-        if even.type == pygame.MOUSEBUTTONDOWN:
+    screen.fill((255, 255, 255))  # Задаем белый фон for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False if event.type == pygame.MOUSEBUTTONDOWN:
             mouse_x, mouse_y = pygame.mouse.get_pos()
             if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y + target_height:
-                target_x = random.randint(0, SCREEN_WIDTH - target_width)
+                score += 1  # Увеличиваем счет на 1 target_x = random.randint(0, SCREEN_WIDTH - target_width)
                 target_y = random.randint(0, SCREEN_HEIGHT - target_height)
 
+    # Перемещение цели
     target_x += target_speed_x
     target_y += target_speed_y
 
+    # Проверка на столкновение с границами экрана
     if target_x <= 0 or target_x >= SCREEN_WIDTH - target_width:
         target_speed_x = -target_speed_x  # Изменяем направление по оси X if target_y <= 0 or target_y >= SCREEN_HEIGHT - target_height:
         target_speed_y = -target_speed_y  # Изменяем направление по оси Y
 
-    screen.blit(target_image, (target_x, target_y))
-    font = pygame.font.Font(None, 36)
+    # Отображение цели screen.blit(target_image, (target_x, target_y))
+
+    # Отображение счета font = pygame.font.Font(None, 36)
     score_text = font.render(f"Очки: {score}", True, (0, 0, 0))
     screen.blit(score_text, (10, 10))
 
     pygame.display.update()
-
 
 pygame.quit()
